4b28ad54b3c8aca6222ed7bcf399ae510c081d5e
Aujla11Akash <aujlaakash1100@gmail.com>
2025-05-27 18:14:23 +0530

    aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -7,23 +7,22 @@
   	<!-- Bootstrap 3.3.7 -->
   	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   	
-  	<!-- Font Awesome -->
+  	
   	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  	<!-- Theme style -->
+  	
   	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  	<!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
+  	
   	<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-    <!-- Magnify -->
+    
     
 
 
-  	<!-- Google Font -->
+  	
   	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-    <!-- Paypal Express -->
+   
     
-  	<!-- Custom CSS -->
+  	
     <style type="text/css">
     /* Small devices (tablets, 768px and up) */
     @media (min-width: 768px){ 
